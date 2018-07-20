01786df1f17efc1cdb6d07f559cd1e10f3f5ffa2
Marina Tassi <mtassi@marinas-workbook.local>
2018-07-20 19:45:08 -0400

    Initial Commit

---
+++ Midas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Midas
+//
+//  Created by Marina Tassi on 7/20/18.
+//  Copyright © 2018 Marina Tassi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
